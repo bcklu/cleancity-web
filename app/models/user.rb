948604ab60afd35c97c0ff2b5bd905887abd2087
Ubuntu <andy@current.starseeders.net>
2010-12-18 10:18:33 +0100

    add: initial import
    ..from https://github.com/fs/rails3-base

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  include User::OmniAuthExtension
+
+  attr_accessible :full_name, :email, :password, :password_confirmation
+  validates :full_name, :presence => true
+
+  def full_name_with_email
+    "#{self[:full_name]} (#{email})"
+  end
+end
