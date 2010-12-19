c506ac59c1fde70f181a9f174f012276f4c00514
Andreas Happe <andreashappe@snikt.net>
2010-12-19 10:41:50 +0100

    fix: check comment's body existence and length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :incident_report
   belongs_to :author, :class_name => 'User'
+  
+  validates_presence_of :body
+  validates_length_of :body, :maximum => 255
 end
