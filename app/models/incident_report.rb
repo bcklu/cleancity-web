790e8bea78fa8f5af3e9324194cfc1a9e5f30d62
Andreas Happe <andreashappe@snikt.net>
2010-12-19 15:17:08 +0100

    add: implement controller side of state machine
    add action IncidentReports#resolve

--- app/models/incident_report.rb
+++ app/models/incident_report.rb
@@ -12,9 +12,10 @@ class IncidentReport < ActiveRecord::Base
   has_many :incident_reports_users, :dependent => :destroy
   has_many :dislikers, :class_name => 'User', :through => :incident_reports_users, :conditions => ["type = ?", "dislike"], :source => :user
   has_many :no_problemers, :class_name => 'User', :through => :incident_reports_users, :conditions => ["type = ?", "not_a_problem"], :source => :user
+  has_many :resolvers, :class_name => 'User', :through => :incident_reports_users, :conditions => ["type = ?", "resolved"], :source => :user
   
   # use geometrical distance
-  named_scope :incidents_within, lambda {
+  scope :incidents_within, lambda {
                 |longitude, latitude, range_x, range_y, limit|
                   where("longitude between ? and ? and latitude between ? and ?",
                         longitude - range_x, longitude + range_x, latitude - range_y, latitude + range_y).limit(limit)
@@ -30,7 +31,7 @@ class IncidentReport < ActiveRecord::Base
     transitions :to => :solved, :from => [:published, :not_a_problem]
   end
   
-  aasm_event :mark_as_no_problem do
+  aasm_event :mark_as_not_a_problem do
     transitions :to => :not_a_problem, :from => [:published]
   end
   
