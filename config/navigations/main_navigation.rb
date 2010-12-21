db29efd010aec3430063c5996d374d7ec20c73eb
Andreas Happe <andreashappe@snikt.net>
2010-12-21 18:38:34 +0100

    chg: switch over to simple-navigation
    This breaks the current layout (as the navigation isn't styled yet)
    but should fix our functionality otherwise.

--- config/navigations/main_navigation.rb
+++ config/navigations/main_navigation.rb
@@ -3,9 +3,21 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     primary.dom_class = 'main'
-
+    
+    # currently the dashboard does nothing, but we might use it later on
     primary.with_options(:if => Proc.new { user_signed_in? }) do |signed_in_user|
       signed_in_user.item :dashboard, 'Dashboard', root_path
     end
+    
+    primary.with_options(:if => Proc.new {current_user.admin?}) do |admin_user|
+      # TODO: add an admin dashboard
+      admin_user.item :admin, 'Admin', admin_incident_reports_path do |admin|
+        admin.item :incidents, 'Incidents', admin_incident_reports_path
+        admin.item :users, 'Users', admin_users_path
+      end
+    end
+    
+    # primary navigation path: incidents
+    primary.item :incidents, 'Incidents', incident_reports_path
   end
 end
