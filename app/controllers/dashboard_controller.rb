c817a13af6d30ad65472652d689f9e253d7c3d6a
Andreas Happe <andreashappe@snikt.net>
2010-12-24 23:40:15 +0100

    del: dashboard controller/views
    this was added by our template and is nothing but bit-rotten code for
    now.

--- app/controllers/dashboard_controller.rb
+++
@@ -1,7 +0,0 @@
-class DashboardController < ApplicationController
-  before_filter :authenticate_user!
-  before_filter :filter_resource_access, :except => [:index, :show]
-
-  def index
-  end
-end
